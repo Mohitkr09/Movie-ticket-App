d0e11d074ce9a9363b3d5d990adbade0fba3b662
Mohitkr09 <mkt27858@gmail.com>
2025-08-11 18:18:26 +0530

    fix payment method

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -6,6 +6,7 @@ import BlurCircle from '../components/BlurCircle'
 import { dateFormat } from '../lib/dateFormat'
 import timeFormat from '../lib/timeFormat'
 import { useAppContext } from '../context/AppContext'
+import { Link } from 'react-router-dom'
 const MyBookings = () => {
   const currency = import.meta.env.VITE_CURRENCY
     const {axios,getToken,user,image_base_url} = useAppContext()
@@ -50,7 +51,7 @@ const MyBookings = () => {
           <div className='flex flex-col md:items-end md:text-right justify-between p-4'>
             <div className='flex items-center gap-4'>
               <p className='text-2xl font-semibold mb-3'>{currency}{item.amount}</p>
-              {!item.isPaid && <button className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>Pay Now</button>}
+              {!item.isPaid && <Link to={item.paymentLink} className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>Pay Now</Link>}
 
             </div>
             <div className='text-sm'>
