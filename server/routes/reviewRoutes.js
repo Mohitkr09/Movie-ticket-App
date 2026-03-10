e5cf93eaa6afe5a9d359972e227174722e6765a9
Mohitkr09 <mkt27858@gmail.com>
2026-03-10 18:12:28 +0530

    add

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { addReview, getReviews } from "../controllers/reviewController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protectAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/", protect, addReview);
+router.post("/", protectAdmin, addReview);
 router.get("/:id", getReviews);
 
 export default router;
